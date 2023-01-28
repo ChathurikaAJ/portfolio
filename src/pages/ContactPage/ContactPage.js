af93fd7a69e4dd4fd73c9163e4aca132b01f3969
Chathurika <chathurika-jayakody.com>
2023-01-27 19:16:32 -0500

    Update contact page

--- src/pages/ContactPage/ContactPage.js
+++ src/pages/ContactPage/ContactPage.js
@@ -14,9 +14,9 @@ export default function ContactPage() {
                 <div className="page__container page__container-left">
                     <p className="page__subtitle">Get In Touch<span className="page__highlight">.</span></p>
                     <div className="contact__container">
-                        <img className="contact__icons" src={github} alt="GitHub"></img>
-                        <img className="contact__icons contact__icons-size" src={linkedin} alt="LinkedIn"></img>
-                        <img className="contact__icons contact__icons-size" src={gmail} alt="Gmail"></img>
+                        <a href="https://github.com/ChathurikaAJ" target="_blank" rel="noreferrer"><img className="contact__icons" src={github} alt="GitHub"></img></a>
+                        <a href="https://www.linkedin.com/in/chathurika-jayakody" target="_blank" rel="noreferrer"><img className="contact__icons contact__icons-size" src={linkedin} alt="LinkedIn"></img></a>
+                        <a href="mailto:chathurika.jayakody@gmail.com"><img className="contact__icons contact__icons-size" src={gmail} alt="Gmail"></img></a>
                     </div>
                 </div>
                 <Nav />
