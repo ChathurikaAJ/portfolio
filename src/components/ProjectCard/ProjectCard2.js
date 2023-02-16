1ee8fb9d9763a6dd15831383d84696f91c82c151
Chathurika <chathurika-jayakody.com>
2023-02-16 12:28:38 -0500

    Update project details

--- src/components/ProjectCard/ProjectCard2.js
+++ src/components/ProjectCard/ProjectCard2.js
@@ -11,7 +11,7 @@ export default function ProjectCard1() {
             <div className="projectcard__container">
                 <img className="projectcard__image" src={colombotodo} alt="Pronounce"></img>
                 <div className="projectcard__details">
-                    <p className="projectcard__info">Blog post about local restaurants, event listings and other fun acitivities that the city of Colombo has to offer.</p>
+                    <p className="projectcard__info">Blog post about local restaurants, event listings and other fun activities that the city of Colombo has to offer.</p>
                     <div className="projectcard__links">
                     <a href="https://github.com/ChathurikaAJ/colombo-to-do-blog" target="_blank" rel="noreferrer"><p className="projectcard__link">Github</p></a>
                     <a href="https://colombotodo.com/" target="_blank" rel="noreferrer"><p className="projectcard__link" >Site</p></a>
